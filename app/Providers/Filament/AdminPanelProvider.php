a7ed4c26fe05e3fbca1e93d54084cff9251e8b89
Hảo Lê <hao.le@opsgreat.vn>
2026-04-02 14:19:58 +0700

    feat: add max content width setting to admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers\Filament;
 
 use Awcodes\ClockWidget\ClockWidget;
 use Filament\Http\Middleware\Authenticate;
+use Filament\Support\Enums\Width;
 use Quochao56\Employee\EmployeePlugin;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -64,6 +65,7 @@ class AdminPanelProvider extends PanelProvider
                 StudentPlugin::make(),
                 PlanningEvaluationPlugin::make(),
             ])
+             ->maxContentWidth(Width::Full)
             ->sidebarCollapsibleOnDesktop(true);
     }
 }
