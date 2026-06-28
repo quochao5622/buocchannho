1b8cc7620ae8bd82ec87ad4468164241d259eefa
quochao5622 <thomaszen63@gmail.com>
2026-06-28 22:08:23 +0700

    feat: add hidden auto-save polling component to filament views

--- resources/views/filament/autosave.blade.php
+++ resources/views/filament/autosave.blade.php
@@ -1,1 +1,1 @@
-<div wire:poll.5s.keep-alive="autoSave" style="position: absolute; width: 1px; height: 1px; opacity: 0; pointer-events: none;"></div>
+<div wire:poll.360s.keep-alive="autoSave" style="position: absolute; width: 1px; height: 1px; opacity: 0; pointer-events: none;"></div>
